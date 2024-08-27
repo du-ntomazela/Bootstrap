3667ce5a568180f218671051c9680bf37e4e7d8d
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-08-26 22:38:41 -0300

    primeiro commit

---
+++ projeto1/main.js
@@ -0,0 +1,6 @@
+function background(){
+    var element = document.body
+    var button = document.getElementById("btn")
+    element.classList.toggle("dark-mode")
+    button.classList.toggle("dark-mode")
+}
